1d9ba9b5349c3022cdf60f9ae4a732c1171c26d7
urasdbwd <102772348+urasdbwd@users.noreply.github.com>
2022-12-12 08:41:37 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,9 @@ secret_key = pyotp.random_base32()
 from flask import jsonify
 
 
-@app.route('/generate', methods=['POST'])
-def generate_otp():
+@app.route('/generate/<secret_key>', methods=['POST','GET'])
+def generate_otp(secret_key):
     # Generate a new OTP for the current time
-    secret_key = request.form['secret_key']
-    print (secret_key)
-
     totp = pyotp.TOTP(secret_key)
     otp = totp.now()
 
@@ -27,6 +24,6 @@ def generate_otp():
     return response
 @app.route('/')
 def gen():
-    return render_template('index.html')
+    return render_template('dc.html')
 if __name__ == '__main__':
     app.run()
